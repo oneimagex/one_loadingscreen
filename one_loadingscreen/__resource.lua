3c0947ecf85239fb20608c10a7ef7f4267c67447
ONEIMAGE/Karim <103147371+oneimagex@users.noreply.github.com>
2022-04-06 22:47:10 +0200

    Add files via upload

---
+++ one_loadingscreen/__resource.lua
@@ -0,0 +1,25 @@
+----------------------------------------------------
+---------------- Made by ONEIMAGE ------------------
+-------------- discord.gg/MAyP7vXXef ---------------
+----------------------------------------------------
+author 'oneimage'
+
+client_script "client.lua"
+
+files {
+  'index.html',
+  'script.js',
+  'player.js',
+  'style.css',
+  'time.js',
+  'loadbar.js',
+  'img/*.png',
+  'images/*.jpg',
+  'music/*.mp3',
+}
+
+loadscreen 'index.html'
+loadscreen_cursor 'yes'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
